09d97d346622ef2de8d0e0923d679a120eb5ccf8
noolisainathareddy <nooli.sainathareddy@gmail.com>
2023-03-24 11:50:43 -0500

    Initial commit

---
+++ chaitanya/src/test/java/com/school/chaitanya/ChaitanyaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.school.chaitanya;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChaitanyaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
